e83f06197419d55527406cc29024c51e0269f3bc
Patricia Segantine <68874361+patriciasegantine@users.noreply.github.com>
2020-12-02 22:36:39 +0000

    upload files!

---
+++ script.js
@@ -0,0 +1,44 @@
+function scrollSmooth() {
+  const internalLinks = document.querySelectorAll('.js-menu a[href^="#"]');
+  if (internalLinks.length) {
+    internalLinks.forEach((link) => {
+      function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+
+        section.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      }
+
+      link.addEventListener('click', scrollToSection);
+    });
+  }
+}
+
+scrollSmooth();
+
+function initScrollSmooth() {
+  const sectionsScroll = document.querySelectorAll('.js-scroll');
+  const halfWindow = window.innerHeight * 0.7;
+
+  if (sectionsScroll.length) {
+    function animaScroll() {
+      sectionsScroll.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - halfWindow < 0;
+        if (isSectionVisible) {
+          section.classList.add('active');
+        } else {
+          section.classList.remove('active');
+        }
+      });
+    }
+
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+
+initScrollSmooth();
